79569fb8cb86547284e9b4bdb4caadd938260a84
Viktor <asvickun@gmail.com>
2018-07-12 16:11:35 -0700

    Some tests

--- MinIterator/MinIterator/Source.cpp
+++ MinIterator/MinIterator/Source.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <algorithm>
+#include "FilteredForwardIterator.h"
+#include <vector>
+
+bool filter(int n)
+{
+	if (n % 2 == 0) return true;
+	return false;
+};
 
 int main()
 {
-
+	std::vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	FilteredForwardIterator<decltype(vec.begin()), bool(int)> first(vec.begin(),vec.end(), filter);
+	FilteredForwardIterator<decltype(vec.end()), bool(int)> second(vec.end(), vec.end(), filter);
+	second=std::min_element(first, second);
+	std::cout << *(*second);
+	system("pause");
 	return 0;
 };

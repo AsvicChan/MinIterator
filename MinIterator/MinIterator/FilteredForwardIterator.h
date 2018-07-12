3673865878ff69a1e64738a146d1a1c209ae0323
Viktor <asvickun@gmail.com>
2018-07-12 16:11:01 -0700

    IT WORKS

--- MinIterator/MinIterator/FilteredForwardIterator.h
+++ MinIterator/MinIterator/FilteredForwardIterator.h
@@ -1,1 +1,80 @@
 #pragma once
+#include <iterator>
+
+template <typename Iterator, typename Filter>
+class FilteredForwardIterator : public Iterator
+{
+private:
+	Iterator iterator_;
+	Iterator end_;
+	Filter* filter_;
+public:
+
+	FilteredForwardIterator(Iterator iterator, Iterator end, Filter filter) : iterator_(iterator), end_(end), filter_(filter) 
+	{
+		if (iterator_ == end_) return;
+		while (!filter_(*iterator_))
+		{
+			++iterator_;
+			if (iterator_ == end_) return;
+		}
+	};
+
+	FilteredForwardIterator(const FilteredForwardIterator& ffi) : iterator_(ffi.iterator_), end_(ffi.end_), filter_(ffi.filter_)
+	{
+
+	};
+
+	FilteredForwardIterator operator=(const FilteredForwardIterator& rhs)
+	{
+		iterator_ = rhs.iterator_;
+		end_ = rhs.end_;
+		filter_ = rhs.filter_;
+		return *this;
+	};
+
+	~FilteredForwardIterator()
+	{
+
+	};
+
+	Iterator operator*() //works
+	{
+		return iterator_;
+	};
+
+	FilteredForwardIterator& operator++() 
+	{
+		if (iterator_ == end_) return *this;
+		++iterator_;
+		if (iterator_ == end_) return *this;
+		while (!filter_(*iterator_))
+		{
+			++iterator_;
+			if (iterator_ == end_) return *this;
+		}
+		return *this;
+	};
+
+	bool operator==(const FilteredForwardIterator& rhs) const 
+	{
+		return iterator_ == rhs.iterator_;
+	};
+
+	bool operator!=(const FilteredForwardIterator& rhs) const 
+	{
+		return iterator_ != rhs.iterator_;
+	};
+
+	FilteredForwardIterator operator++(int)
+	{
+		FilteredForwardIterator tmp(*this); 
+		operator++(); 
+		return tmp;
+	};
+
+	FilteredForwardIterator()
+	{
+
+	};
+};
